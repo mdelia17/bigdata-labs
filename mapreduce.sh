e7055cc783cf1424c8f6212033d908418228c6e8
mdelia17 <marcodelia1900@gmail.com>
2021-04-16 20:12:39 +0200

    improved mapreduce script

--- mapreduce.sh
+++ mapreduce.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
+FILE=/$1/$2
+
 usage()
 {
 	echo "Usage: mapreduce.sh <mapreduce job> <input file in hdfs>"
 	exit 1
 }
 
+missing_input()
+{
+	echo "Wrong input file!"
+	exit 1
+}
+
+missing_mr()
+{
+	echo "Mapper and/or Reducer are/is missing!"
+	exit 1
+}
+
 if [ $# -eq 0 ]; then
     usage
+fi
+
+if ! [ -f "$1/$2" ]; then
+	missing_input
+fi
+
+if ! { [ -f "$1/mapper.py" ] && [ -f "$1/reducer.py" ]; }; then
+	missing_mr
 fi
 
 $HADOOP_HOME/bin/hdfs dfs -put bigdata/$1/$2 input
